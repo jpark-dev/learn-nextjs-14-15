7fed3bf5be06dca6b31fedcdf220e1474fc03b2b
Jason Park <zeipar@gmail.com>
2025-08-17 02:16:00 -0700

    Practice NextJS 14/15 updates.

---
+++ components/navigation.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function Navigation() {
+  const path = usePathname();
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link href="/">Home</Link> {path === "/" ? "hehe" : "not home"}
+        </li>
+        <li>
+          <Link href="/about-us">About Us</Link> {path === "/about-us" ? "about us?" : "not about us"}
+        </li>
+      </ul>
+    </nav>
+  );
+}
