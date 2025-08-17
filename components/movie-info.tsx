7c51fc658cbaf44218d5e3b7526e122a9bb55c5e
Jason Park <zeipar@gmail.com>
2025-08-17 03:35:01 -0700

    Installed missing ESlint packages, separted export for URL link and replaced img with Next Image for better LCP loading.

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,4 +1,5 @@
-import { API_URL } from '../app/(home)/page';
+import Image from 'next/image';
+import { API_URL } from '../constants';
 import styles from '../styles/movie-info.module.css';
 
 export async function getMovie(id: string) {
@@ -10,7 +11,11 @@ export default async function MovieInfo({ id }: { id: string }) {
   const movie = await getMovie(id);
   return (
     <div className={styles.container}>
-      <img src={movie.poster_path} className={styles.poster} />
+      <Image
+        src={movie.poster_path}
+        className={styles.poster}
+        alt={movie.title}
+      />
       <div className={styles.info}>
         <h1 className={styles.title}>{movie.title}</h1>
         <h3>⭐️ {movie.vote_average.toFixed(1)}</h3>
